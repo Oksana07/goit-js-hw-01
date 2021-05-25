9f878fd2a8413e95f0327e92ca3d0c50ba661aff
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2021-05-25 14:35:14 +0300

    Коміт

--- js/hw-1.js
+++ js/hw-1.js
@@ -1,5 +1,29 @@
+// переша задача
+// const productName = "Droid";
 
-const balance = 1000;
-const messag = balance >= 0 ?
-    "Позитивний баланс" : "Негативний баланс";
-console.log(messag);
+// console.log(productName);
+// 'Droid'
+
+// const pricePerItem = 2000;
+
+// console.log(pricePerItem);
+// 2000
+
+
+// друга задача
+// let productName = "Droid";
+// productName = "Repair droid";
+// let pricePerItem = 2000;
+// pricePerItem = 3500;
+
+
+// третя задача
+const topSpeed = 160;
+const distance = 617.54;
+let opSpeed = 160;
+let istance = 617.54;
+
+const login = "mango935";
+
+const isOnline = true;
+const isAdmin = false;
