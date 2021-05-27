298abac9083c9a01d05cdeb5b44eae889f1c6d8f
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2021-05-27 17:41:01 +0300

    Коміт

--- js/hw-1.js
+++ js/hw-1.js
@@ -65,10 +65,10 @@
 // console.log(add(5, 10, 15));
 
 // десята задача
-function makeMessage (name, price) {
-   const message = `You picked ${name}, price per item is ${price} credits`;
-  return message;
-}
+// function makeMessage (name, price) {
+//    const message = `You picked ${name}, price per item is ${price} credits`;
+//   return message;
+// }
 
 // одинадцята задача
 // function calculateTotalPrice (orderedQuantity, pricePerItem) {
@@ -115,10 +115,97 @@ function makeMessage (name, price) {
 // console.log(37 >= "18");
 
 // 
-function isAdult(age) {
-  // Change code below this line
-  const passed = age >= 18;
+// function isAdult(age) {
+//   // Change code below this line
+//   const passed = age >= 18;
 
-  // Change code above this line
-  return passed;
+//   // Change code above this line
+//   return passed;
+
+// чотирнадцята задача
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = 'jqueryismyjam';
+//   const isMatch = password === SAVED_PASSWORD;
+//   return isMatch;
+// }
+
+// пятнадцята задача
+// function checkAge(age) {
+//   let message;
+//   if (age >= "20") { 
+//     message = 'You are an adult';
+//   } else {
+//     message = 'You are a minor';
+//   }
+
+// //   return message;
+// }
+
+// for (let i = 0; i < 10; i += 1) {
+//   console.log(i);
+// }
+// console.log(`qweqwe`)
+
+
+// шістнадцята задача
+// function checkStorage(available, ordered) {
+//     let message;
+  
+//     if (ordered > available) {
+//         message = `Not enough goods in stock!`
+
+//     } else {
+//         message = `Order is processed, our manager will contact you.`
+//     }
+
+//     return (message);
+// }
+// checkStorage(100, 50);
+// checkStorage(100, 130);
+// checkStorage(200, 20);
+// checkStorage(200, 150);
+
+// сімнадцята задача
+// Задание
+// Замени выражения со стандартными математеческими операторами на комбинированный оператор присвоения с добавлением, вычитанием, умножением и делением.
+
+// Тесты
+// Значение переменной a равно 7
+// Использован оператор +=
+// Значение переменной b равно 6
+// Использован оператор -=
+// Значение переменной c равно 45
+// Использован оператор *=
+// Значение переменной d равно 2
+// Использован оператор /=
+  
+//   // Розвязок
+  
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
+// вісімнадцята задача
+function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+  let message;
+let totalPrice = pricePerDroid * orderedQuantity;
+ if (totalPrice > customerCredits) {
+   message = "Insufficient funds!";
+   }
+  else {
+    totalPrice = customerCredits - totalPrice;
+   message = `You ordered ${orderedQuantity} droids, you have ${totalPrice} credits left`;
 }
+consol.log(message);
+  }
+  makeTransaction(3000, 5, 23000);
+  makeTransaction(1000, 3, 15000);
+  makeTransaction(5000, 10, 8000);
+  makeTransaction(2000, 8, 10000);
+  makeTransaction(500, 10, 5000);
