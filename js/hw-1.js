6180d2fe52a0d11ad91aac510203faf8d695914e
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2021-05-27 11:40:33 +0300

    Коміт

--- js/hw-1.js
+++ js/hw-1.js
@@ -18,12 +18,107 @@
 
 
 // третя задача
-const topSpeed = 160;
-const distance = 617.54;
-let opSpeed = 160;
-let istance = 617.54;
+// const topSpeed = 160;
+// const distance = 617.54;
+// let opSpeed = 160;
+// let istance = 617.54;
 
-const login = "mango935";
+// const login = "mango935";
 
-const isOnline = true;
-const isAdmin = false;
+// const isOnline = true;
+// const isAdmin = false;
+
+// шоста задача
+// const pricePerDroid  = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+// сьома задача
+// function sayHi() {
+  
+//   console.log("Hello, this is my first function!");
+// }
+// sayHi();
+
+// восьма задача
+// function add(a, b, c) {
+//   console.log("Addition result equals 52");
+//   console.log("Addition result equals 60");
+//   console.log("Addition result equals 30");
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// девята задача
+// function add(a, b, c) {
+// return a + b + c;
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// десята задача
+function makeMessage (name, price) {
+   const message = `You picked ${name}, price per item is ${price} credits`;
+  return message;
+}
+
+// одинадцята задача
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//   // Change code below this line
+//   const totalPrice = orderedQuantity * pricePerItem;
+
+//   // Change code above this line
+//   return totalPrice;
+// };
+
+// // дванадцята задача
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+// const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+//  const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+//   return message;
+// }
+
+// ТРИНАДЦЯТА ЗАДАЧА
+// function isAdult(age) {
+//   // Change code below this line
+//  const passed = 20 >= 18;
+
+//   // Change code above this line
+//   return passed;
+// }
+// console.log(20 >= "18");
+
+// function isAdult(age) {
+//  const passed = 14 >= 18;
+//   return passed;
+// }
+// console.log(14 >= "18");
+
+// function isAdult(age) {
+//  const passed = 8 >= 18;
+//  return passed;
+// }
+// console.log(8 >= "18");
+
+// function isAdult(age) {
+//  const passed = 37 >= 18;
+//   return passed;
+// }
+// console.log(37 >= "18");
+
+// 
+function isAdult(age) {
+  // Change code below this line
+  const passed = age >= 18;
+
+  // Change code above this line
+  return passed;
+}
