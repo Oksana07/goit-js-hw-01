4b75575337725f431225c4c0d50be33d2a59c128
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2021-06-09 17:40:09 +0300

    Комит

--- js/hw-3.js
+++ js/hw-3.js
@@ -132,4 +132,68 @@
 
 // 16
 
+// console.log(countTotalSalary({})); // 0
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 })) // 330
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 })); // 400
+
+// function countTotalSalary(obj) {
+//   let totalSalary = 0;
+//   for (const salary of Object.values(obj)) {
+//     totalSalary += salary;
+//   }
+//   return totalSalary;
+// }
+
+// 17
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (let color of colors) {
+//  hexColors.push(color.hex);
+//  rgbColors.push(color.rgb); 
+// }
+
+// 18
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (let product of products){
+//    if  (product.name === productName)
+//   return product.price;
+//     }
+// return null;
+//   // Change code above this line
+// }
+
+// 19
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  const arr = [];
+ for (const product of products) {
+   if (product[propName]) {
+  arr.push(product[propName])
+    }
+ }
+return arr;
+  // Change code above this line
+}
 
