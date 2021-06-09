d3e24ad8560f5c671efb07f1148e3f2a659958a7
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2021-06-09 16:30:39 +0300

    Комыт

--- js/hw-3.js
+++ js/hw-3.js
@@ -56,4 +56,80 @@
 // values.push(apartment[key]);
 // };
 
+
 // 11
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: "apt",
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//  if (apartment.hasOwnProperty(key)) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+//  }
+// }
+
+// 12
+// function countProps(object) {
+//   let propCount = 0;
+//   for (const item in object) {
+//     if (object.hasOwnProperty(item)) {
+//       propCount += 1;
+//     }
+//   }
+//   return propCount;
+// }
+
+// console.log(countProps({})); //0
+// countProps({ name: "Mango", age: 2 }); //2
+// countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }); //3
+
+// 13
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+
+// for (key of keys) {
+//   // Ключ
+//   values.push(apartment[key]);
+//   // Значение свойства
+//   console.log(apartment[values]);
+// }
+
+// // 14
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+//   const keys = Object.keys(object);
+//   console.log(keys);
+
+//   for (const key of keys) {
+//       propCount += key.length;
+//     }
+//   return propCount;
+// }
+
+// // 15
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// const values = Object.values(apartment);
+
+// 16
+
+
